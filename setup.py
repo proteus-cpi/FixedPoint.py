9244ef4cc3d08a58a64f44acb3efad8854630d1b
Santhosh K Pilakkat <pilakkat@gmail.com>
2014-02-23 21:52:03 +0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fixedpoint',
+      version='0.12',
+      description='Based on Tim Peter\'s fixedpoint',
+      author='proteus-cpi',
+      author_email='proteus-cpi@xxx',
+      url='http://none',
+      packages=['fixedpoint'],
+     )
